b937d1e38da24e5b4f7993ae7fe665def2052dc5
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2025-02-17 17:59:27 +0100

    Launch in browser

--- app.R
+++ app.R
@@ -365,4 +365,4 @@ server = function(input, output, session) {
 
 
 # Run the application
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
